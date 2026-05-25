126725f73b626201b5ed11b3b360dcbe395c5bf5
Anil Marneni <anilmarneni30@gmail.com>
2026-05-25 20:53:29 +0530

    Problem of the Day 25th May 2026 Done

---
+++ May/25-05-2026/LeetCode.cpp
@@ -0,0 +1,61 @@
+/*
+1871. Jump Game VII
+Medium
+You are given a 0-indexed binary string s and two integers minJump and maxJump. In the beginning, you are standing at index 0, which is equal to '0'. You can move from index i to index j if the following conditions are fulfilled:
+i + minJump <= j <= min(i + maxJump, s.length - 1), and
+s[j] == '0'.
+Return true if you can reach index s.length - 1 in s, or false otherwise.
+
+Example 1:
+Input: s = "011010", minJump = 2, maxJump = 3
+Output: true
+Explanation:
+In the first step, move from index 0 to index 3. 
+In the second step, move from index 3 to index 5.
+
+Example 2:
+Input: s = "01101110", minJump = 2, maxJump = 3
+Output: false
+
+Constraints:
+2 <= s.length <= 105
+s[i] is either '0' or '1'.
+s[0] == '0'
+1 <= minJump <= maxJump < s.length
+*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool canReach(string s, int minJump, int maxJump) {
+    int n = s.size();
+
+    vector<int> dp(n, 0);
+    dp[0] = 1;
+
+    int reachable = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (i - minJump >= 0)
+            reachable += dp[i - minJump];
+
+        if (i - maxJump - 1 >= 0)
+            reachable -= dp[i - maxJump - 1];
+
+        dp[i] = (reachable > 0 && s[i] == '0');
+    }
+
+    return dp[n - 1];
+}
+
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+    int minJump, maxJump;
+    cin >> minJump >> maxJump;
+    cout << canReach(s, minJump, maxJump) << endl;
+    return 0;
+}
